b8f4753e4c29b838bb285fb901fd98c51dd3d8a1
Galdino <mateusgaldino1000@hotmail.com>
2023-01-14 14:12:05 -0300

    finish base cinematic configs and camera updates

---
+++ Assets/Scripts/CInematics/CinematicControlRemover.cs
@@ -0,0 +1,33 @@
+using RPG.Control;
+using RPG.Core;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicControlRemover : MonoBehaviour
+    {
+        PlayableDirector director;
+        GameObject player;
+
+        private void Start()
+        {
+            director = GetComponent<PlayableDirector>();
+            director.played += DisableControl;
+            director.stopped += EnableControl;
+
+            player = GameObject.FindWithTag("Player");
+        }
+
+        void DisableControl(PlayableDirector director)
+        {
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
+        }
+
+        void EnableControl(PlayableDirector director)
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+    }
+}
